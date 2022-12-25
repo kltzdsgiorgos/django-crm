cb03e59f2082a37ac4ff4552694c4ad42f437acd
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2022-12-25 15:29:43 +0200

    templates

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    context = {
+        "name": 'John',
+        "age": '22',
+    }
+    return render(request, "second_page.html", context)
